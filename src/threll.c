32e4f5fa00ad78d63f24fce6110e87bd373b2d11
Default User <laurence.a.maddox@gmail.com>
2017-09-06 14:27:23 -0500

    trying to construct a pipeline of lightweight "server" threads for stream processing

--- src/threll.c
+++ src/threll.c
@@ -252,11 +252,12 @@ static int command (pipeline_t *cmd, fd_t **input, bool first, bool last) {
 
 	/* TODO a buffer array queue that will allow for
 	 * input_{esz,n} != output_{esz,n} */
-	if (cmd->input_esz != cmd->output_esz)
+	/*if (cmd->input_esz != cmd->output_esz)
 		return -2;
 	if (cmd->input_n != cmd->output_n)
 		return -3;
-	threll_pipe (pipettes + 0, pipettes + 1, cmd->input_esz, cmd->input_n);
+	threll_pipe (pipettes + 0, pipettes + 1, cmd->input_esz, cmd->input_n);*/
+	threll_pipe (pipettes + 0, pipettes + 1, cmd->output_esz, cmd->output_n);
 	/*(void) pipe (pipettes);*/
 
 	cargs.first = first;
