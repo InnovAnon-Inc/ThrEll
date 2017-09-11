c1d30ee991b9d209cb96f6577a4f4983e93fa274
Default User <laurence.a.maddox@gmail.com>
2017-09-11 11:34:56 -0500

    cross compiling for android

--- autogen.sh
+++ autogen.sh
@@ -17,6 +17,7 @@ if [ $# -eq 0 ] ; then exit 0
 else
 	test "x$1" == xmake
 	DIR="$(readlink -f `pwd`)"
+	rm -fr ../ThrEll-build
 	mkdir ../ThrEll-build
 	cd ../ThrEll-build
 	"$DIR/configure" "--prefix=$HOME"
