72fc894361ce96979846e6dacb3bb082b0375965
Default User <laurence.a.maddox@gmail.com>
2017-09-21 09:50:03 -0500

    bugfix

--- src/main.c
+++ src/main.c
@@ -36,6 +36,9 @@ static int rdcb (
 	buffer_t const *restrict buf_in,
 	void *restrict arg) {
 	TODO (?)
+	buf_out->n = min (buf_in->n, buf_out->n);
+	memcpy (buf_out, buf_in, buf_out->n);
+	return 0;
 }
 
 __attribute__ ((nonnull (1, 2), nothrow, warn_unused_result))
@@ -44,14 +47,44 @@ static int wrcb (
    buffer_t const *restrict buf_in,
    void *restrict arg) {
 	TODO (?)
+	buf_out->n = min (buf_in->n, buf_out->n);
+	memcpy (buf_out, buf_in, buf_out->n);
+	return 0;
+}
+
+	#pragma GCC diagnostic push
+	#pragma GCC diagnostic ignored "-Wpadded"
+typedef struct {
+	io_t *restrict io;
+	fd_t rd;
+	fd_t wr;
+} rw_io_t;
+	#pragma GCC diagnostic pop
+
+void *rw_io_wrapper (void *restrict _arg) {
+	rw_io_t *restrict arg = (rw_io_t *restrict) _arg;
+	error_check (rw_io (arg->io, arg->rd, arg->wr) != 0)
+		return NULL;
+	return NULL;
 }
 
 int main (void) {
 	size_t nargv = 3;
 	thserver_t argvs[3];
+	size_t in_bufsz = 10;
+	size_t in_nbuf  = 3;
+	size_t out_bufsz = 8;
+	size_t out_nbuf  = 3;
+	pthread_t io_thread;
+	rw_io_t rw_io_arg;
+
+	io_t io;
+	error_check (alloc_io (&io, in_bufsz, in_nbuf, out_bufsz, out_nbuf) != 0)
+		return EXIT_FAILURE;
+
 	argvs[0].cb = rdcb;
-	argvs[0].input_esz = 0;
-	argvs[0].input_n   = 0;
+	argvs[0].input_esz = in_bufsz;
+	argvs[0].input_n   = in_nbuf;
 	argvs[0].output_esz = sizeof (int);
 	argvs[0].output_n   = 2;
 
@@ -64,10 +97,19 @@ int main (void) {
 	argvs[2].cb = wrcb;
 	argvs[2].input_esz = argvs[1].output_esz;
 	argvs[2].input_n   = argvs[1].input_n;
-	argvs[2].output_esz = 0;
-	argvs[2].output_n   = 0;
+	argvs[2].output_esz = out_bufsz;
+	argvs[2].output_n   = out_nbuf;
 
-	if (exec_pipeline (argvs, nargv) != 0) return EXIT_FAILURE;
+	rw_io (&io, STDIN_FILENO, STDOUT_FILENO);
+	error_check (pthread_create (
+		&io_thread, NULL, rw_io_wrapper, &rw_io_arg) != 0)
+		return EXIT_FAILURE;
+
+	error_check (exec_pipeline (argvs, nargv, io.in, io.out) != 0) return EXIT_FAILURE;
+
+	error_check (pthread_join (io_thread, NULL) != 0) return EXIT_FAILURE;
+
+	error_check (free_io (&io) != 0) return EXIT_FAILURE;
 
 	return EXIT_SUCCESS;
 }
